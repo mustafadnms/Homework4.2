664de3bb098389d48bc9bb6f78ee032f2838df9b
mustafadnms <donmusmustafa@gmail.com>
2021-05-03 23:08:25 +0300

    4.gün 2.ödev

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1, "Engin", "Demiroð", new Date(1985,1,6), "28861499108"));
+		
+	}
+
+}
